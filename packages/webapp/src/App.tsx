f766918805271d908508536349d5ac3d629ede1e
mayjunjuly0313 <tank6138@gmail.com>
2021-07-08 22:37:59 +0900

    feat: added oauth2 google login button

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import * as queryString from 'query-string'
+// generate a url that asks permissions for Blogger and Google Calendar scopes
+const scopes = [
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile',
+]
+
+const stringifiedParams = queryString.stringify({
+  client_id: process.env.REACT_APP_GOOGLE_OAUTH2_CLIENT_ID,
+  redirect_uri: process.env.REACT_APP_GOOGLE_OAUTH2_REDIRECT_URL,
+  scope: scopes.join(' '),
+  response_type: 'code',
+  access_type: 'offline',
+  prompt: 'consent',
+})
+
+const oauth2Url = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
+console.log(process.env.REACT_APP_GOOGLE_OAUTH2_CLIENT_ID)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <a href={oauth2Url}>google login</a>
 }
 
-export default App;
+export default App
